fb605a419288a37ee95430768d6d6b62bd67fc7b
sd8capricon <72347505+sd8capricon@users.noreply.github.com>
2023-06-24 23:03:53 +0530

    links file

---
+++ src/utils/links.ts
@@ -0,0 +1,14 @@
+const links = {
+    dbit: "https://www.dbit.in/",
+    dbitACM: "https://dbit.acm.org/",
+    teknack: "https://dbit.acm.org/teknack/",
+    flockflair: "https://dbit.acm.org/flockflair/",
+    instagram: "https://www.instagram.com/acmdbit/",
+    linkedIn: "https://www.linkedin.com/company/acm-dbit/",
+    github: "https://github.com/acm-dbit",
+    twitter: "https://twitter.com/acmdbit?lang=en",
+    youtube: "https://www.youtube.com/channel/UC58zaYJo0LL_aJXvqLDJGLA",
+    facebook: "https://www.facebook.com/ACMDBIT/"
+}
+
+export default links
